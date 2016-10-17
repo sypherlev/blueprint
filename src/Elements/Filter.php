738a4043c334d5834ab99247ba43986c6617c846
Claire Ryan <cryan@pcis.com>
2016-10-17 16:10:06 -0700

    Swapped callable commands between Filters and Patterns

--- src/Elements/Filter.php
+++ src/Elements/Filter.php
@@ -7,9 +7,7 @@ use SypherLev\Blueprint\QueryBuilders\SourceInterface;
 class Filter
 {
     private $wheres = [];
-    private $order = [];
-    private $limit = [];
-    private $group;
+    private $joins = [];
 
     public function where(Array $where, $innercondition = 'AND', $outercondition = 'AND') {
         $this->wheres[] = array(
@@ -20,40 +18,22 @@ class Filter
         return $this;
     }
 
-    public function orderBy($columnname_or_columnarray, $order = 'ASC', $useAliases = false) {
-        $this->order = array(
-            'columns' => $columnname_or_columnarray,
-            'order' => $order,
-            'aliases' => $useAliases
+    public function join($firsttable, $secondtable, Array $on, $type = 'inner') {
+        $this->joins[] = array(
+            'firsttable' => $firsttable,
+            'secondtable' => $secondtable,
+            'on' => $on,
+            'type' => $type
         );
-        return $this;
-    }
-
-    public function limit($rows, $offset = false) {
-        $this->limit = array(
-            'rows' => $rows,
-            'offset' => $offset
-        );
-        return $this;
-    }
-
-    public function groupBy($columnname_or_columnarray) {
-        $this->group = $columnname_or_columnarray;
         return $this;
     }
 
     public function setSourceParams(SourceInterface $source) {
-        if(!empty($this->order)) {
-            $source->orderBy($this->order['columns'], $this->order['order'], $this->order['aliases']);
-        }
-        if(!empty($this->limit)) {
-            $source->limit($this->limit['rows'], $this->limit['offset']);
+        foreach ($this->joins as $join) {
+            $source->join($join['firsttable'], $join['secondtable'], $join['on'], $join['type']);
         }
         foreach ($this->wheres as $where) {
             $source->where($where['where'], $where['inner'], $where['outer']);
-        }
-        if(!empty($this->group)) {
-            $source->groupBy($this->group);
         }
         return $source;
     }
--- src/Elements/Pattern.php
+++ src/Elements/Pattern.php
@@ -9,6 +9,9 @@ class Pattern
     private $table;
     private $columns;
     private $joins = [];
+    private $order = [];
+    private $limit = [];
+    private $group;
 
     public function table($tableName) {
         $this->table = $tableName;
@@ -30,12 +33,46 @@ class Pattern
         return $this;
     }
 
+    public function orderBy($columnname_or_columnarray, $order = 'ASC', $useAliases = false) {
+        $this->order = array(
+            'columns' => $columnname_or_columnarray,
+            'order' => $order,
+            'aliases' => $useAliases
+        );
+        return $this;
+    }
+
+    public function limit($rows, $offset = false) {
+        $this->limit = array(
+            'rows' => $rows,
+            'offset' => $offset
+        );
+        return $this;
+    }
+
+    public function groupBy($columnname_or_columnarray) {
+        $this->group = $columnname_or_columnarray;
+        return $this;
+    }
+
     public function setSourceParams(SourceInterface $source) {
         $source->table($this->table);
+
+        $source->columns($this->columns);
+
         foreach ($this->joins as $join) {
             $source->join($join['firsttable'], $join['secondtable'], $join['on'], $join['type']);
         }
-        $source->columns($this->columns);
+
+        if(!empty($this->order)) {
+            $source->orderBy($this->order['columns'], $this->order['order'], $this->order['aliases']);
+        }
+        if(!empty($this->limit)) {
+            $source->limit($this->limit['rows'], $this->limit['offset']);
+        }
+        if(!empty($this->group)) {
+            $source->groupBy($this->group);
+        }
         return $source;
     }
 }
